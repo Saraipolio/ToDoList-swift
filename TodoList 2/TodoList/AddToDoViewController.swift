be6be2f3a6345129165950d673e2c724f547bff9
Saraipolio <107435063+Saraipolio@users.noreply.github.com>
2023-05-05 15:52:32 -0400

    Add files via upload

---
+++ TodoList 2/TodoList/AddToDoViewController.swift
@@ -0,0 +1,64 @@
+//
+//  AddToDoViewController.swift
+//  TodoList
+//
+//  Created by Student on 2/17/23.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    var toDoTableViewController: ToDoTableViewController?=nil
+    var pickerController = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var prioritySegment: UISegmentedControl!
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerController.delegate = self
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            imageView.image = image
+        }
+        pickerController.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func mediaFolderTapped(_ sender: Any) {
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        {
+            let newToDo = ToDoCD(context:context)
+            newToDo.priority = Int32(prioritySegment.selectedSegmentIndex)
+            
+            if let name = nameTextField.text {
+                newToDo.name = name
+            }
+            newToDo.image = imageView.image?.jpegData(compressionQuality: 1.0)
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
